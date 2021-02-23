708417db10b153f1dda8cf472ac0eee9c008c221
CypressAdam <adam@cypress.io>
2021-02-23 14:05:02 -0500

    basics added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,260 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="text-xxxxl">Walnut</h1>
+        <p className="text-l">This is a design system for software testing interfaces.</p>
       </header>
+      <section className="bg-white section">
+        <h2 className="text-l text-metal-60">Palette</h2>
+        <h3 className="text-m text-metal-90">Metal</h3>
+        <div className="color-blocks text-xs">
+          <div className="color-block bg-metal-05 text-metal-90">
+            Metal 05
+          </div>
+          <div className="color-block bg-metal-10 text-metal-90">
+            Metal 10
+          </div>
+          <div className="color-block bg-metal-20 text-metal-90">
+            Metal 20
+          </div>
+          <div className="color-block bg-metal-30 text-metal-90">
+            Metal 30
+          </div>
+          <div className="color-block bg-metal-40 text-metal-90">
+            Metal 40
+          </div>
+          <div className="color-block bg-metal-50 text-metal-05">
+            Metal 50
+          </div>
+          <div className="color-block bg-metal-60 text-metal-05">
+            Metal 60
+          </div>
+          <div className="color-block bg-metal-70 text-metal-05">
+            Metal 70
+          </div>
+          <div className="color-block bg-metal-80 text-metal-05">
+            Metal 80
+          </div>
+          <div className="color-block bg-metal-90 text-metal-05">
+            Metal 90
+          </div>
+        </div>
+        <h3 className="text-m text-red-70">Red</h3>
+        <div className="color-blocks text-xs">
+          <div className="color-block bg-red-40 text-white">
+            Red 40
+          </div>
+          <div className="color-block bg-red-50 text-white">
+            Red 50
+          </div>
+          <div className="color-block bg-red-60 text-white">
+            Red 60
+          </div>
+          <div className="color-block bg-red-70 text-white">
+            Red 70
+          </div>
+        </div>
+        <h3 className="text-m text-chill-70">Chill</h3>
+        <div className="color-blocks text-xs">
+          <div className="color-block bg-chill-05 text-chill-90">
+            Chill 05
+          </div>
+          <div className="color-block bg-chill-10 text-chill-90">
+            Chill 10
+          </div>
+          <div className="color-block bg-chill-20 text-chill-90">
+            Chill 20
+          </div>
+          <div className="color-block bg-chill-30 text-chill-90">
+            Chill 30
+          </div>
+          <div className="color-block bg-chill-40 text-chill-90">
+            Chill 40
+          </div>
+          <div className="color-block bg-chill-50 text-chill-05">
+            Chill 50
+          </div>
+          <div className="color-block bg-chill-60 text-chill-05">
+            Chill 60
+          </div>
+          <div className="color-block bg-chill-70 text-chill-05">
+            Chill 70
+          </div>
+          <div className="color-block bg-chill-80 text-chill-05">
+            Chill 80
+          </div>
+          <div className="color-block bg-chill-90 text-chill-05">
+            Chill 90
+          </div>
+        </div>
+        <h3 className="text-m text-olive-60">Olive</h3>
+        <div className="color-blocks text-xs">
+          <div className="color-block bg-olive-05 text-olive-90">
+            olive 05
+          </div>
+          <div className="color-block bg-olive-10 text-olive-90">
+            olive 10
+          </div>
+          <div className="color-block bg-olive-20 text-olive-90">
+            olive 20
+          </div>
+          <div className="color-block bg-olive-30 text-olive-90">
+            olive 30
+          </div>
+          <div className="color-block bg-olive-40 text-olive-90">
+            olive 40
+          </div>
+          <div className="color-block bg-olive-50 text-olive-05">
+            olive 50
+          </div>
+          <div className="color-block bg-olive-60 text-olive-05">
+            olive 60
+          </div>
+        </div>
+        <h3 className="text-m text-papaya-60">Papaya</h3>
+        <div className="color-blocks text-xs">
+          <div className="color-block bg-papaya-05 text-papaya-90">
+            papaya 05
+          </div>
+          <div className="color-block bg-papaya-10 text-papaya-90">
+            papaya 10
+          </div>
+          <div className="color-block bg-papaya-20 text-papaya-90">
+            papaya 20
+          </div>
+          <div className="color-block bg-papaya-30 text-papaya-90">
+            papaya 30
+          </div>
+          <div className="color-block bg-papaya-40 text-papaya-90">
+            papaya 40
+          </div>
+          <div className="color-block bg-papaya-50 text-papaya-05">
+            papaya 50
+          </div>
+          <div className="color-block bg-papaya-60 text-papaya-05">
+            papaya 60
+          </div>
+        </div>
+        <h3 className="text-m text-green-60">Green</h3>
+        <div className="color-blocks text-xs">
+          <div className="color-block bg-green-05 text-green-90">
+            green 05
+          </div>
+          <div className="color-block bg-green-10 text-green-90">
+            green 10
+          </div>
+          <div className="color-block bg-green-20 text-green-90">
+            green 20
+          </div>
+          <div className="color-block bg-green-30 text-green-90">
+            green 30
+          </div>
+          <div className="color-block bg-green-40 text-green-90">
+            green 40
+          </div>
+          <div className="color-block bg-green-50 text-green-05">
+            green 50
+          </div>
+          <div className="color-block bg-green-60 text-green-05">
+            green 60
+          </div>
+        </div>
+        <h3 className="text-m text-brand-01">Brand &amp; Accents</h3>
+        <div className="color-blocks text-xs">
+          <div className="color-block bg-brand-00 text-white">
+            Brand 00
+          </div>
+          <div className="color-block bg-brand-01 text-white">
+            Brand 01
+          </div>
+          <div className="color-block bg-accent-00 text-white">
+            Accent 00
+          </div>
+          <div className="color-block bg-accent-01 text-metal-90">
+            Accent 01
+          </div>
+          <div className="color-block bg-accent-02 text-metal-90">
+            Accent 02
+          </div>
+
+        </div>
+      </section>
+      <section className="bg-white section">
+        <h2 className="text-l text-metal-60">Typography</h2>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">XXXXL</div>
+          <div className="text-xxxxl">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">XXXL</div>
+          <div className="text-xxxl">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">XXL</div>
+          <div className="text-xxl">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">XL</div>
+          <div className="text-xl">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">L</div>
+          <div className="text-l">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">M</div>
+          <div className="text-m">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">BASE</div>
+          <div className="text-base text-metal-70">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">S</div>
+          <div className="text-s text-metal-70">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">XS</div>
+          <div className="text-xs text-metal-70">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">S CAPS</div>
+          <div className="text-s-caps text-metal-70">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+
+        <div className="type">
+          <div className="text-mono-s text-metal-40">MONO BASE</div>
+          <div className="text-mono-base text-metal-70">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+        <div className="type">
+          <div className="text-mono-s text-metal-40">MONO S</div>
+          <div className="text-mono-s text-metal-70">
+            The five boxing wizards jump quickly
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 }
